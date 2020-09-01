39b1cc5faad615071f9b303df2a4e5a289549296
Hayden Faulds <fauldsh@gmail.com>
2020-09-01 17:30:12 +0100

    extract Map

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,18 @@
 // src/lib.rs
 #![recursion_limit = "1024"]
+mod map;
+
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use web_sys::CanvasRenderingContext2d;
 use web_sys::HtmlCanvasElement;
-use web_sys::HtmlImageElement;
 use yew::prelude::*;
 use yew::services::{RenderService, Task};
-use std::collections::HashMap;
 
 struct Hello {
   canvas: Option<HtmlCanvasElement>,
   ctx: Option<CanvasRenderingContext2d>,
-  images: HashMap<String, HtmlImageElement>,
+  map: map::Map,
   link: ComponentLink<Self>,
   node_ref: NodeRef,
   render_loop: Option<Box<dyn Task>>,
@@ -31,7 +31,7 @@ impl Component for Hello {
     Self {
       canvas: None,
       ctx: None,
-      images: load_images(),
+      map: map::Map::random(10, 10),
       link,
       node_ref: NodeRef::default(),
       render_loop: None,
@@ -43,13 +43,14 @@ impl Component for Hello {
       Msg::Render(_) => {
         let ctx = self.ctx.as_ref().expect("Context not initialized!");
 
-        for y in 0..9 {
-          for x in 0..9 {
+        let map = &self.map;
+        for y in 0..map.columns() {
+          for x in 0..map.rows() {
             ctx
               .draw_image_with_html_image_element_and_dw_and_dh(
-                &self.images.get("grass_S.png").expect(""),
-                x as f64 * 92. + (y % 2) as f64 * 46.,
-                y as f64 * 46.,
+                map.get(x, y),
+                x as f64 * 90. + (y % 2) as f64 * 45.,
+                y as f64 * 45.,
                 250.,
                 250.,
               )
@@ -83,13 +84,8 @@ impl Component for Hello {
     self.ctx = Some(ctx);
 
     if first_render {
-      // The callback to request animation frame is passed a time value which can be used for
-      // rendering motion independent of the framerate which may vary.
       let render_frame = self.link.callback(Msg::Render);
       let handle = RenderService::request_animation_frame(render_frame);
-
-      // A reference to the handle must be stored, otherwise it is dropped and the render won't
-      // occur.
       self.render_loop = Some(Box::new(handle));
     }
   }
@@ -103,81 +99,6 @@ impl Component for Hello {
       </section>
     }
   }
-}
-
-fn load_images() -> HashMap<String, HtmlImageElement> {
-  let images = vec![
-    "building_mine_S.png",
-    "water_S.png",
-    "building_water_S.png",
-    "sand_S.png",
-    "unit_houseLarge_S.png",
-    "path_end_S.png",
-    "grass_hill_S.png",
-    "grass_forest_S.png",
-    "dirt_lumber_S.png",
-    "stone_S.png",
-    "stone_rocks_S.png",
-    "river_cornerSharp_S.png",
-    "path_straight_S.png",
-    "river_straight_S.png",
-    "path_cornerSharp_S.png",
-    "river_intersectionH_S.png",
-    "path_intersectionH_S.png",
-    "dirt_S.png",
-    "unit_tower_S.png",
-    "building_market_S.png",
-    "building_house_S.png",
-    "building_tower_S.png",
-    "building_smelter_S.png",
-    "stone_hill_S.png",
-    "unit_house_S.png",
-    "unit_mill_S.png",
-    "path_corner_S.png",
-    "building_cabin_S.png",
-    "building_wall_S.png",
-    "path_start_S.png",
-    "building_dock_S.png",
-    "river_crossing_S.png",
-    "stone_mountain_S.png",
-    "building_village_S.png",
-    "path_crossing_S.png",
-    "building_farm_S.png",
-    "path_intersectionB_S.png",
-    "river_intersectionB_S.png",
-    "river_intersectionF_S.png",
-    "path_intersectionF_S.png",
-    "unit_wallTower_S.png",
-    "building_mill_S.png",
-    "river_intersectionD_S.png",
-    "path_intersectionD_S.png",
-    "grass_S.png",
-    "river_intersectionC_S.png",
-    "path_intersectionC_S.png",
-    "river_intersectionA_S.png",
-    "path_intersectionA_S.png",
-    "building_sheep_S.png",
-    "sand_rocks_S.png",
-    "building_castle_S.png",
-    "unit_boat_S.png",
-    "path_intersectionE_S.png",
-    "water_island_S.png",
-    "river_intersectionE_S.png",
-    "river_end_S.png",
-    "unit_tree_S.png",
-    "water_rocks_S.png",
-    "river_corner_S.png",
-    "path_intersectionG_S.png",
-    "river_intersectionG_S.png",
-    "river_start_S.png",
-  ];
-  let mut elements: HashMap<String, HtmlImageElement> = HashMap::new();
-  for image in images {
-    let element = HtmlImageElement::new().unwrap();
-    element.set_src(&format!("./tiles/{}", image));
-    elements.insert(String::from(image), element);
-  }
-  elements
 }
 
 #[wasm_bindgen(start)]
---
+++ src/map.rs
@@ -0,0 +1,131 @@
+// src/map.rs
+use std::collections::HashMap;
+use rand::seq::SliceRandom;
+use web_sys::HtmlImageElement;
+
+pub struct Map {
+  tiles: Vec<Vec<HtmlImageElement>>,
+}
+
+impl<'a> Map {
+  pub fn random(x: u8, y: u8) -> Map {
+    let tiles = Tiles::load();
+    let mut rows = Vec::new();
+    for _ in 1..x {
+      let mut row = Vec::new();
+      for _ in 1..y {
+        row.push(tiles.random().clone())
+      }
+      rows.push(row);
+    }
+    Map { tiles: rows }
+  }
+
+  pub fn rows(&self) -> u8 {
+    self.tiles.len() as u8
+  }
+
+  pub fn columns(&self) -> u8 {
+    self.tiles.get(0).unwrap().len() as u8
+  }
+
+  pub fn get(&'a self, x: u8, y: u8) -> &'a HtmlImageElement {
+    self.tiles.get(x as usize).unwrap().get(y as usize).unwrap()
+  }
+}
+
+pub struct Tiles {
+  map: HashMap<String, HtmlImageElement>,
+  vec: Vec<HtmlImageElement>,
+}
+
+impl Tiles {
+  pub fn load() -> Tiles {
+    let images = vec![
+      "building_mine_S.png",
+      "water_S.png",
+      "building_water_S.png",
+      "sand_S.png",
+      //"unit_houseLarge_S.png",
+      //"path_end_S.png",
+      "grass_hill_S.png",
+      "grass_forest_S.png",
+      "dirt_lumber_S.png",
+      "stone_S.png",
+      "stone_rocks_S.png",
+      //"river_cornerSharp_S.png",
+      //"path_straight_S.png",
+      //"river_straight_S.png",
+      //"path_cornerSharp_S.png",
+      //"river_intersectionH_S.png",
+      //"path_intersectionH_S.png",
+      "dirt_S.png",
+      //"unit_tower_S.png",
+      "building_market_S.png",
+      "building_house_S.png",
+      "building_tower_S.png",
+      "building_smelter_S.png",
+      "stone_hill_S.png",
+      //"unit_house_S.png",
+      //"unit_mill_S.png",
+      //"path_corner_S.png",
+      "building_cabin_S.png",
+      "building_wall_S.png",
+      //"path_start_S.png",
+      "building_dock_S.png",
+      //"river_crossing_S.png",
+      "stone_mountain_S.png",
+      "building_village_S.png",
+      //"path_crossing_S.png",
+      "building_farm_S.png",
+      //"path_intersectionB_S.png",
+      //"river_intersectionB_S.png",
+      //"river_intersectionF_S.png",
+      //"path_intersectionF_S.png",
+      //"unit_wallTower_S.png",
+      "building_mill_S.png",
+      //"river_intersectionD_S.png",
+      //"path_intersectionD_S.png",
+      "grass_S.png",
+      //"river_intersectionC_S.png",
+      //"path_intersectionC_S.png",
+      //"river_intersectionA_S.png",
+      //"path_intersectionA_S.png",
+      "building_sheep_S.png",
+      "sand_rocks_S.png",
+      "building_castle_S.png",
+      //"unit_boat_S.png",
+      //"path_intersectionE_S.png",
+      "water_island_S.png",
+      //"river_intersectionE_S.png",
+      //"river_end_S.png",
+      //"unit_tree_S.png",
+      "water_rocks_S.png",
+      //"river_corner_S.png",
+      //"path_intersectionG_S.png",
+      //"river_intersectionG_S.png",
+      //"river_start_S.png",
+      ];
+    let mut map: HashMap<String, HtmlImageElement> = HashMap::new();
+    let mut vec: Vec<HtmlImageElement> = Vec::new();
+
+    for image in images {
+      let element = HtmlImageElement::new().unwrap();
+      element.set_src(&format!("./tiles/{}", image));
+      map.insert(String::from(image), element.clone());
+      vec.push(element);
+    }
+
+    Tiles { map, vec }
+  }
+}
+
+impl<'a> Tiles {
+  pub fn get(&'a self, name: & str) -> &'a HtmlImageElement {
+    self.map.get(name).unwrap()
+  }
+
+  pub fn random(&'a self) -> &'a HtmlImageElement {
+    self.vec.choose(&mut rand::thread_rng()).unwrap()
+  }
+}
